8467b210183cdbceeafe578750d7ad743221cdbd
Kshitij <kshtjkmr2@gmail.com>
2018-08-13 20:50:18 +0530

    List Added

---
+++ CollectionFramework/src/ListInterface/ArrayListDemo1.java
@@ -0,0 +1,18 @@
+package ListInterface;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo1 {
+
+	public static void main(String[] args) {
+		ArrayList list = new ArrayList();
+		list.add(3);
+		list.add(5);
+		list.add("Kshitij");
+		list.add(null);
+		list.add(2);
+		System.out.println(list);
+
+	}
+
+}
---
+++ CollectionFramework/src/ListInterface/LinkedListDemo1.java
@@ -0,0 +1,18 @@
+package ListInterface;
+
+import java.util.LinkedList;
+
+public class LinkedListDemo1 {
+
+	public static void main(String[] args) {
+		LinkedList list = new LinkedList();
+		list.add(5);
+		list.add(1);
+		list.add("Khushi");
+		list.add(null);
+		list.add(9);
+		System.out.println(list);
+
+	}
+
+}
