87caa3804da129f02ee012ed70f6f7de04b00744
Kshitij <kshtjkmr2@gmail.com>
2018-08-14 13:03:37 +0530

    insert modified and drop inserted

--- JavaDataBase/src/crudoperation/DropTable.java
+++ JavaDataBase/src/crudoperation/DropTable.java
@@ -3,20 +3,18 @@ package crudoperation;
 import java.sql.Connection;
 import java.sql.Statement;
 
-public class InsertDataInTable {
+public class DropTable {
 
 	public static void main(String[] args) {
 		Connection con = null;
-		Statement st = null; 
+		Statement st = null;
 		try {
-			con=JDBCUtil.getOracleConnection();
-			String query = "insert into student values(103,'Anup','anup2@gmail.com',78,'Chandauli',8762523453)";
-			st=con.createStatement();
-			int res=st.executeUpdate(query);
-			if(res!=0){
-				System.out.println("Data inserted");
-			}
-			
+			con = JDBCUtil.getOracleConnection();
+			String qString = "drop table employee";
+			st = con.createStatement();
+			int res = st.executeUpdate(qString);
+			if(res!=0)
+				System.out.println("table deleted");
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
